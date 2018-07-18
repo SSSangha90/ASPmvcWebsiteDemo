8a5cbda29acb0909ccec65f825cec16d6ccd2157
Sukh Sangha <37102418+SSSangha90@users.noreply.github.com>
2018-07-18 15:58:02 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,52 @@
+﻿using partyInvitationJuly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace partyInvitationJuly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+       [HttpPost]
+       public ActionResult RsvpForm(guestResponse guestResponse)
+        {
+            //to do: send email to party organiser 
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else //if there has been an error
+            {
+                return View();
+            }
+          
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
